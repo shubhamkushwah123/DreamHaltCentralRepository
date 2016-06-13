b82ce4a98e98dae540e03f2e0c8f7d166ed73209
shubhamkushwah123 <shubhamkushwah123@gmail.com>
2016-06-13 15:13:34 +0200

    Hibernate Dependency and test flow added

---
+++ DreamHaltDao/src/test/java/test/dh/dao/implementations/HibernateTest.java
@@ -0,0 +1,32 @@
+package test.dh.dao.implementations;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.dh.dao.entities.Test;
+
+public class HibernateTest {
+	
+	public static void main(String args[])
+	{
+		Test t = new Test();
+		t.setName("Shubham");
+		
+		@SuppressWarnings("deprecation")
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		
+		Session session = sessionFactory.openSession();
+		
+		session.beginTransaction();
+		
+		session.save(t);
+		
+		session.getTransaction().commit();
+		
+		session.close();
+		
+		
+	}
+
+}
